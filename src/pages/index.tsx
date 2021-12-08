a74f67aba288ec741dcc85fc912aace4922b7a1a
Marcos Nespolo <marcos.nesp@gmail.com>
2021-12-07 23:39:41 -0300

    fix components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from 'next'
 import React from 'react'
-import PrimaryButton from '../components/Buttons/PrimaryButton'
-import Card from '../components/Cards/Card'
-import InputText from '../components/Inputs/InputText'
 
 const Home: NextPage = () => {
   return (
