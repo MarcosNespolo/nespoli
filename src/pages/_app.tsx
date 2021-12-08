1a81f1c0b467c6fa8c97f61d7b4ceb19d072c586
Marcos Nespolo <marcos.nesp@gmail.com>
2021-12-07 23:32:27 -0300

    font size

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,13 +10,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <div className="flex flex-col justify-between h-screen w-full bg-gradient-to-b bg-opacity-10 from-black to-primary-dark">
-        <span className="self-center mt-12 antialiased font-extralight text-blue-50 tracking-widest text-xl ">Em breve</span>
+        <span className="self-center mt-12 antialiased font-extralight text-blue-50 tracking-widest text-md ">Em breve</span>
         <div className="flec flex-col self-center w-full content-center">
-          <span className="block w-full text-center mb-4 antialiased font-extralight text-blue-100 text-8xl tracking-widest">Nespoli</span>
-          <span className="block w-full text-center antialiased font-thin text-blue-50 tracking-widest text-3xl ">Software Development</span>
+          <span className="block w-full text-center mb-4 antialiased font-extralight text-blue-100 text-6xl tracking-widest">Nespoli</span>
+          <span className="block w-full text-center antialiased font-thin text-blue-50 tracking-widest text-xl ">Software Development</span>
           <Component {...pageProps} />
         </div>
-        <span className="self-center mb-12 antialiased font-extralight text-blue-50 tracking-widest text-xl ">contato@nespoli.dev</span>
+        <span className="self-center mb-12 antialiased font-extralight text-blue-50 tracking-widest text-md ">contato@nespoli.dev</span>
       </div>
     </>
   )
